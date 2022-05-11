32bce3ef32818886e10829271f95da2e48c2af77
Evert Bekaert <bekaertevert@gmail.com>
2022-05-11 17:05:22 +0200

    Improve agent-evert with masks

--- Zeeslag-2/Assets/Scripts/Agents/Agent_Evert.cs
+++ Zeeslag-2/Assets/Scripts/Agents/Agent_Evert.cs
@@ -13,13 +13,13 @@ public class Agent_Evert : Agent
 
     private bool firstStart = true;
     private List<Vector2> shotCoords;
-    private bool checkCoordinates;
 
-    public override void OnEpisodeBegin()
+    public override void OnEpisodeBegin() //called first
     {
+        shotResult = new char();
         shotCoords = new List<Vector2>();
-        checkCoordinates = false;
         transform.localPosition = new Vector3(0,transform.localPosition.y,0);
+        chosenCoordinates = new Vector2(0, 0);
 
         if (firstStart)
         {
@@ -32,118 +32,26 @@ public class Agent_Evert : Agent
         }
     }
 
-    public override void CollectObservations(VectorSensor sensor)
+    public override void CollectObservations(VectorSensor sensor) //called fter episode begin
     {
         sensor.AddObservation(chosenCoordinates);
         sensor.AddObservation(shotResult);       
     }
 
-    public override void WriteDiscreteActionMask(IDiscreteActionMask actionMask)
+    public override void WriteDiscreteActionMask(IDiscreteActionMask actionMask) //called after collect observations
     {
-        //check if player can shoot (uses timer) or if player chose already existing coordinates
-        if (checkCoordinates)
-        {
-            if (Game.Player2CanShoot == false || shotCoords.Contains(chosenCoordinates) || Game.GameRestarted == false)
-            {
-                if (shotCoords.Contains(chosenCoordinates))
-                {
-                    Debug.Log("tried to fire twice, disabling firing");
-                }
-                actionMask.SetActionEnabled(2, 1, false);
-            }
-            else
-            {
-                actionMask.SetActionEnabled(2, 1, true);                
-            }
-            checkCoordinates = false;
-        }
-        else
-        {
-            actionMask.SetActionEnabled(2, 1, false);
-        }
-        
-        if (!checkCoordinates)
-        {
-            //check if player reached the edge
-            if (transform.localPosition.x == Game.FieldPlayer1.Size - 1)
-            {
-                actionMask.SetActionEnabled(0, 2, false);
-            }
-            else
-            {
-                actionMask.SetActionEnabled(0, 2, true);
-            }
+        //check if player can shoot (uses configurable timer) or if player chose already existing coordinates
+        TryShoot(actionMask);
 
-            if (transform.localPosition.x == 0)
-            {
-                actionMask.SetActionEnabled(0, 0, false);
-            }
-            else
-            {
-                actionMask.SetActionEnabled(0, 0, true);
-            }
-
-            if (transform.localPosition.z == Game.FieldPlayer1.Size - 1)
-            {
-                actionMask.SetActionEnabled(1, 2, false);
-            }
-            else
-            {
-                actionMask.SetActionEnabled(1, 2, true);
-            }
-
-            if (transform.localPosition.z == 0)
-            {
-                actionMask.SetActionEnabled(1, 0, false);
-            }
-            else
-            {
-                actionMask.SetActionEnabled(1, 0, true);
-            }
-        }
-        else
-        {
-            actionMask.SetActionEnabled(0, 0, false);
-            actionMask.SetActionEnabled(1, 0, false);
-            actionMask.SetActionEnabled(0, 2, false);
-            actionMask.SetActionEnabled(1, 2, false);
-        }
+        //check if player reached the edge
+        TryMove(actionMask);
     }
 
-    public override void OnActionReceived(ActionBuffers actionBuffers)
+    public override void OnActionReceived(ActionBuffers actionBuffers) //called last
     {
-        switch (actionBuffers.DiscreteActions[0])
-        {
-            case 0:
-                transform.localPosition = new Vector3(transform.localPosition.x - 1, transform.localPosition.y, transform.localPosition.z);
-                break;
-            case 1:
-                transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y, transform.localPosition.z);
-                break;
-            case 2:
-                transform.localPosition = new Vector3(transform.localPosition.x + 1, transform.localPosition.y, transform.localPosition.z);
-                break;
-        }
-
-        switch (actionBuffers.DiscreteActions[1])
-        {
-            case 0:
-                transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y, transform.localPosition.z - 1);
-                break;
-            case 1:
-                transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y, transform.localPosition.z);
-                break;
-            case 2:
-                transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y, transform.localPosition.z + 1);
-                break;
-        }
-        transform.localPosition = new Vector3(actionBuffers.DiscreteActions[0], transform.localPosition.y, actionBuffers.DiscreteActions[1]);
-
-        chosenCoordinates = new Vector2(transform.localPosition.x, transform.localPosition.z);
-        checkCoordinates = true;
-
+        //try to fire
         if (actionBuffers.DiscreteActions[2] == 1)
-        {           
+        {
             if (Game.Player2CanShoot)
             {
                 Debug.Log("Player fired in time");
@@ -156,47 +64,98 @@ public class Agent_Evert : Agent
             char lastResult = shotResult;
             shotResult = Game.Player2Shoot(chosenCoordinates);
             shotCoords.Add(chosenCoordinates);
-                
 
-            if (shotResult == 'S')
+            switch (shotResult)
             {
-                if (lastResult == 'S')
-                {
-                    Debug.Log("Agent hit 2 in a row!");
-                    AddReward(2.0f); //2 hits in a row
-                }
-                else
-                {
-                    Debug.Log("Agent hit a ship!");
-                    AddReward(1.0f);
-                }
-            }
-            else if (shotResult == 'W')
-            {
-                Debug.Log("Agent hit water!");
-                AddReward(-0.5f);
-            }
-            else if (shotResult == 'M' || shotResult == 'H')
-            {
-                Debug.Log("Agent hit same target twice");
-                //AddReward(-1f);
-                //EndEpisode();
-            }
-
-            if (Game.GameState == GameState.Completed)
-            {
-                if (Game.winner == Winner.Player2)
-                {
-                    Debug.Log("Agent won the game");
-                    AddReward(100.0f);
-                }
-                else if (Game.winner == Winner.Player1)
-                {
-                    Debug.Log("Agent lost the game");
-                }
-                EndEpisode();
-            }
+                case 'S':                    
+                    Debug.Log("Agent hit a ship");
+                    if (lastResult == 'S')
+                    {
+                        Debug.Log("Agent hit 2 in a row");
+                        AddReward(1.0f);
+                    }                    
+                    break;
+                case 'W':
+                    Debug.Log("Agent hit water");
+                    AddReward(-1.0f);
+                    break;
+                case 'M':
+                case 'H':
+                    Debug.Log("Agent hit same target twice");
+                    break;
+            }            
         }
-        
+
+        //try to move
+        int x = actionBuffers.DiscreteActions[0];
+        int y = actionBuffers.DiscreteActions[1];
+        transform.localPosition += new Vector3(x == 0 ? -1 : x == 2 ? 1 : 0, 0, y == 0 ? -1 : y == 2 ? 1 : 0);
+        chosenCoordinates = new Vector2(transform.localPosition.x, transform.localPosition.z);
+
+        //check if game is completed
+        if (Game.GameState == GameState.Completed)
+        {
+            if (Game.winner == Winner.Player2)
+            {
+                Debug.Log("Agent won the game");
+                AddReward(100.0f);
+            }
+            else if (Game.winner == Winner.Player1)
+            {
+                Debug.Log("Agent lost the game");
+            }
+            EndEpisode();
+        }
+    }
+
+    private void TryMove(IDiscreteActionMask actionMask)
+    {
+        if (transform.localPosition.x >= Game.FieldPlayer1.Size - 1)
+        {
+            actionMask.SetActionEnabled(0, 2, false);
+        }
+        else
+        {
+            actionMask.SetActionEnabled(0, 2, true);
+        }
+
+        if (transform.localPosition.x <= 0)
+        {
+            actionMask.SetActionEnabled(0, 0, false);
+        }
+        else
+        {
+            actionMask.SetActionEnabled(0, 0, true);
+        }
+
+        if (transform.localPosition.z >= Game.FieldPlayer1.Size - 1)
+        {
+            actionMask.SetActionEnabled(1, 2, false);
+        }
+        else
+        {
+            actionMask.SetActionEnabled(1, 2, true);
+        }
+
+        if (transform.localPosition.z <= 0)
+        {
+            actionMask.SetActionEnabled(1, 0, false);
+        }
+        else
+        {
+            actionMask.SetActionEnabled(1, 0, true);
+        }
+    }
+
+    private void TryShoot(IDiscreteActionMask actionMask)
+    {
+        if (Game.Player2CanShoot == false || shotCoords.Contains(chosenCoordinates) || Game.GameRestarted == false || transform.localPosition.x < 0 || transform.localPosition.y > Game.FieldPlayer1.Size-1 || transform.localPosition.z < 0 || transform.localPosition.z > Game.FieldPlayer1.Size-1)
+        {
+            actionMask.SetActionEnabled(2, 1, false);
+        }
+        else
+        {
+            actionMask.SetActionEnabled(2, 1, true);
+        }
     }
 }
