ceddea7b7f09ec026504b0c79a70f753eec219a8
BjornFelix <bjorn11.felix@gmail.com>
2022-05-22 16:32:24 +0200

    Add cannon aiming

--- Zeeslag-2/Assets/Scripts/Ship Behavior/CannonTest.cs
+++ Zeeslag-2/Assets/Scripts/Ship Behavior/CannonTest.cs
@@ -1,32 +1,29 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System.Timers;
-using System.Threading.Tasks;
-using System.Collections;
 
-public class Cannon : MonoBehaviour
+public class CannonTest : MonoBehaviour
 {
+
     private List<AudioClip> shootSounds;
     private Transform turret;
+    public Transform target;
     private Transform ShotPoint;
     private GameObject bullet;
     private float BlastPower;
-
-    private Vector3 target;
-
-
-    private void Start()
+    // Start is called before the first frame update
+    void Start()
     {
-        shootSounds = GameObject.Find("AudioHelper").GetComponent<AudioHelper>().ShootSounds;
+        // shootSounds = GameObject.Find("AudioHelper").GetComponent<AudioHelper>().ShootSounds;
         bullet = GameObject.Find("Bullet");
         turret = transform.GetChild(0);
         ShotPoint = transform.GetChild(1);
     }
 
-
-    private void Update()
+    // Update is called once per frame
+    void Update()
     {
-        RotateTowardsTarget(this.target);
+        Shoot(target.position);
     }
 
     public void RotateTowardsTarget(Vector3 target)
@@ -38,9 +35,9 @@ public class Cannon : MonoBehaviour
         transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime);
         StartCoroutine(RotateCannon(target));
     }
+
     private IEnumerator RotateCannon(Vector3 target)
     {
-
         yield return new WaitForSeconds(2f);
         var lookPos = target - transform.position;
         lookPos.y = 0;
@@ -52,39 +49,35 @@ public class Cannon : MonoBehaviour
         rotation *= Quaternion.Euler(-20, 0, 0);
         turret.rotation = Quaternion.Slerp(turret.rotation, rotation, Time.deltaTime);
         yield return new WaitForSeconds(2f);
-
-
-
-
-
+        FireBullet(target);
 
     }
-
-
-
     public void FireBullet(Vector3 target)
     {
-        enabled = false;
- 
         double distancex = target.x - transform.position.x;
         double distancez = target.z - transform.position.z;
+
         double sqh = distancex * distancex + distancez * distancez;
+
         var distance = System.Math.Sqrt(sqh);
+
+
+
         BlastPower = (float)System.Math.Sqrt((distance * 9.81));
+
+
         GameObject CreatedCannonball = Instantiate(bullet, ShotPoint.position, ShotPoint.rotation * Quaternion.Euler(45, 0, 0));
         CreatedCannonball.GetComponent<Rigidbody>().velocity = ShotPoint.transform.up * BlastPower;
 
-
-
     }
+
+
     public void Shoot(Vector3 target, bool missed = false)
     {
-
-        this.target = target;
-        enabled = true;
+        RotateTowardsTarget(target);
         AudioPlayer audioPlayer = Instantiate(GameObject.Find("AudioPlayer"), transform.position, Quaternion.identity).GetComponent<AudioPlayer>();
         audioPlayer.Play(shootSounds[Random.Range(0, shootSounds.Count)]);
-        FireBullet(target);
+
         //if (missed) projectile.gameObject.tag = "MissBullet"
         //if (!missed) projectile.gameObject.tag = "HitBullet"
     }
