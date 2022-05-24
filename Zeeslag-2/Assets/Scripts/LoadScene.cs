9a899638ead336f4e6ac735b0ca4ddf36b7d66dc
RubenVorsselmans <78792844+RubenVorsselmans@users.noreply.github.com>
2022-05-24 11:14:36 +0200

    changed canvas script

--- Zeeslag-2/Assets/Scripts/LoadScene.cs
+++ Zeeslag-2/Assets/Scripts/LoadScene.cs
@@ -6,28 +6,46 @@ using UnityEngine.SceneManagement;
 public class LoadScene : MonoBehaviour
 {
     public GameObject pauseCanvas;
-    public void _LoadScene(string name)
-    {
-        SceneManager.LoadScene(name);
-    }
+    public GameObject mainMenuCanvas;
+    public GameObject victoryCanvas;
+    public GameObject gameOverCanvas;
     public void doExitGame()
     {
         Application.Quit();
     }
-    public void Resume()
+    public void StartGame()
     {
-        Time.timeScale = 1;
+        mainMenuCanvas.SetActive(false);
         pauseCanvas.SetActive(false);
+        victoryCanvas.SetActive(false);
+        gameOverCanvas.SetActive(false);
     }
     public void Pause()
     {
-        Time.timeScale = 0;
         Vector3 vHeadPos = Camera.main.transform.position;
         Vector3 vGazeDir = Camera.main.transform.forward;
         pauseCanvas.transform.position = (vHeadPos + vGazeDir * 3.0f) + new Vector3(0.0f, -.40f, 0.0f);
-        Vector3 vRot = Camera.main.transform.eulerAngles; vRot.z = 0;
+        Vector3 vRot = Camera.main.transform.eulerAngles;
+        vRot.z = 0;
         pauseCanvas.transform.eulerAngles = vRot;
         pauseCanvas.SetActive(true);
+        victoryCanvas.SetActive(false);
+        gameOverCanvas.SetActive(false);
+        mainMenuCanvas.SetActive(false);
     }
-
-}
+    public void mainMenu()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        mainMenuCanvas.SetActive(true);
+        pauseCanvas.SetActive(false);
+        victoryCanvas.SetActive(false);
+        gameOverCanvas.SetActive(false);
+    }
+    private void Update()
+    {
+        if(mainMenuCanvas.activeInHierarchy == true || pauseCanvas.activeInHierarchy == true)
+            Time.timeScale = 0;
+        else
+            Time.timeScale = 1;
+    }
+}   
